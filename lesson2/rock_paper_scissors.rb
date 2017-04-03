3f3945a647ce9df5d1fd4a00f51788aa321f9caf
Skyler Olds <skylerolds@gmail.com>
2017-04-02 22:44:19 -0600

    Try to satisfy Rubocop

--- lesson2/rock_paper_scissors.rb
+++ lesson2/rock_paper_scissors.rb
@@ -1,6 +1,6 @@
 STANDARD = %w[rock paper scissors]
 BIG_BANG_THEORY = %w[rock paper scissors lizard spock]
-THAT_70s_SHOW = %w[nuke cockroach boot]
+THAT_70S_SHOW = %w[nuke cockroach boot]
 ROUNDS = 5
 WINNING_SCORE = (ROUNDS / 2.0).ceil
 
@@ -13,29 +13,36 @@ def valid_input?(input)
 end
 
 def first_beats_second?(first, second)
-  (first == VALID_CHOICES[0] && (second == VALID_CHOICES[2] || second == VALID_CHOICES[3])) ||
-  (first == VALID_CHOICES[1] && (second == VALID_CHOICES[0] || second == VALID_CHOICES[4])) ||
-  (first == VALID_CHOICES[2] && (second == VALID_CHOICES[1] || second == VALID_CHOICES[3])) ||
-  (first == VALID_CHOICES[3] && (second == VALID_CHOICES[4] || second == VALID_CHOICES[1])) ||
-  (first == VALID_CHOICES[4] && (second == VALID_CHOICES[2] || second == VALID_CHOICES[0]))
+  (first == VALID_CHOICES[0] &&
+  (second == VALID_CHOICES[2] || second == VALID_CHOICES[3])) ||
+  (first == VALID_CHOICES[1] &&
+  (second == VALID_CHOICES[0] || second == VALID_CHOICES[4])) ||
+  (first == VALID_CHOICES[2] &&
+  (second == VALID_CHOICES[1] || second == VALID_CHOICES[3])) ||
+  (first == VALID_CHOICES[3] &&
+  (second == VALID_CHOICES[4] || second == VALID_CHOICES[1])) ||
+  (first == VALID_CHOICES[4] &&
+  (second == VALID_CHOICES[2] || second == VALID_CHOICES[0]))
 end
 
 def get_choice
   player_choice = ''
-    loop do
-      prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-      player_choice = gets.chomp.downcase
-      if valid_input?(player_choice)
-        VALID_CHOICES.each do |word| 
-          if word.start_with?(player_choice)
-            player_choice = word
-          end
+  
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    player_choice = gets.chomp.downcase
+    if valid_input?(player_choice)
+      VALID_CHOICES.each do |word|
+        if word.start_with?(player_choice)
+          player_choice = word
         end
-        break
-      else
-        prompt("That's not a valid choice.")
       end
+      break
+    else
+      prompt("That's not a valid choice.")
     end
+  end
+
   player_choice
 end
 
@@ -48,14 +55,15 @@ answer = gets.chomp
 playing = !(answer =~ /^n/i)
 
 while playing
-  prompt("Choose your ruleset: 1) Standard, 2) Big Bang Theory, 3) That 70's Show")
+  prompt("Choose your ruleset:")
+  prompt("1) Standard, 2) Big Bang Theory, 3) That 70's Show")
   answer = gets.chomp
 
   if answer == '2' || answer.downcase == 'big bang theory'
     VALID_CHOICES = BIG_BANG_THEORY
     prompt("Choosing Big Bang Theory ruleset...")
   elsif answer == '3' || answer.downcase == "that 70's show"
-    VALID_CHOICES = THAT_70s_SHOW
+    VALID_CHOICES = THAT_70S_SHOW
     prompt("Choosing That 70's Show ruleset...")
   else
     VALID_CHOICES = STANDARD
@@ -81,16 +89,16 @@ while playing
     break if player_score >= WINNING_SCORE || computer_score >= WINNING_SCORE
 
     prompt("You: #{player_score}, Computer: #{computer_score}")
-    
+
     puts
     prompt('Play another round? [Yn]')
     break if gets.chomp =~ /^n/i
   end
 
   if player_score > computer_score
-    prompt("Winner! You won #{player_score} out of the #{ROUNDS} rounds. Congrats!")
+    prompt("Winner! You won #{player_score} out of the #{ROUNDS} rounds.")
   else
-    prompt("Loser! You lost #{player_score} out of the #{ROUNDS} rounds. Sorry!")
+    prompt("Loser! You lost #{player_score} out of the #{ROUNDS} rounds.")
   end
 
   prompt('Continue playing? [Yn]')
